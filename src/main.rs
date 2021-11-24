ed3bd660ae3aea7a1c9c11f2e17c30384ed347b7
youbo <9277206@gmail.com>
2021-11-24 22:53:05 +0800

    feat: 禁用标准库，提供入口点

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+/// panic 时会调用这个方法
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    // 要求返回 !
+    loop {}
+}
+
+/// 程序入口点. no_mangle 避免 _start 函数名被重写
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
